838f36874d7d331bf8c7f5606de6c3b6f45e009b
Nathan Faria <nathanfaria607@gmail.com>
2023-06-02 11:31:29 -0300

    feat: configure builder.Services to use in memory db

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add db in this case im using in memory db with Microsoft.EntityFrameworkCore.InMemory
+builder.Services.AddDbContext<ApiDbContext>(options => 
+    options.UseInMemoryDatabase("TasksDB")
+);
 
 var app = builder.Build();
 
@@ -27,4 +34,10 @@ class Task
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsCompleted { get; set; }  
+}
+
+class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+    public DbSet<Task> Tasks => Set<Task>();
 }
