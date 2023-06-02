5e0827297275f0dc36d180948b9285421ec1afaa
Nathan Faria <nathanfaria607@gmail.com>
2023-06-02 11:23:56 -0300

    feat: add Microsoft.EntityFrameworkCore.InMemory with nuget pm and add model Class Task

--- Program.cs
+++ Program.cs
@@ -21,3 +21,10 @@ app.MapGet("phrases", async () =>
 
 
 app.Run();
+
+class Task
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsCompleted { get; set; }  
+}
